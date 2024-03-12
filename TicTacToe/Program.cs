d6a8166d49b7481fa5924edfb8030162c107bd40
= <=>
2024-03-12 08:34:11 -0700

    Agregar archivos de proyecto.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static void DibujarTablero()
+        {
+
+        }
+        public static void DibujarJugador(char jugador)
+        {
+
+        }
+        public static bool EsValidoElMovimiento(char[,] tablero, int pos)
+        {
+            return false;
+        }
+
+    }
+}
